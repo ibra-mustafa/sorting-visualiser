2e5a13a631286e34da62474f3236f98150f07799
Ibrahim Mustafa <59354268+ibra-mustafa@users.noreply.github.com>
2023-08-21 23:37:58 +0300

    Added mergesort butto

--- src/app/(sorting visualiser)/layout.tsx
+++ src/app/(sorting visualiser)/layout.tsx
@@ -7,9 +7,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body><div className="bg-grey-300 sm:row-span-2 row-span-3 w-screen grid grid-flow-row">
-        Nav{/* <NavButtons onAlgoChange={onAlgoChange} settings={setting} sort={sort}/> */}
-      </div>
+      <body><nav className="grid grid-flow-row bg-grey-300 sm:row-span-2 row-span-3 w-screen grid grid-flow-row">
+        <div className='flex items-center justify-center w-full'>
+          <button>Merge Sort</button>
+        </div>
+        </nav>
       {children}</body>
     </html>
   )
