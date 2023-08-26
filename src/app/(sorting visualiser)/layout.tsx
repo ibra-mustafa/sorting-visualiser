e2e8d13a153f2f16ceee595c415cbe30840e43c7
Ibrahim Mustafa <59354268+ibra-mustafa@users.noreply.github.com>
2023-08-26 23:58:42 +0300

    added other buttons and enhanced styles

--- src/app/(sorting visualiser)/layout.tsx
+++ src/app/(sorting visualiser)/layout.tsx
@@ -8,8 +8,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body><nav className="grid grid-flow-row bg-grey-300 sm:row-span-2 row-span-3 w-screen grid grid-flow-row">
-        <div className='flex items-center justify-center w-full'>
-          <button>Merge Sort</button>
+        <div className='p-4 gap-4 flex items-center justify-center w-full'>
+          <button className='border border-teal-100 p-2 '>Merge Sort</button>
+          <button className='border border-teal-100 p-2 '>Insertion Sort</button>
+          <button className='underline border border-teal-100 p-2 '>Sort !</button>
         </div>
         </nav>
       {children}</body>
