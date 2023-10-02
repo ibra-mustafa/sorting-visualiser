a200f1b0b281eb80584f5f41245e77338b6d6d35
Ibrahim Mustafa <59354268+ibra-mustafa@users.noreply.github.com>
2023-10-02 23:34:21 +0300

    Update AlgoContext.tsx
    Added type and initial values to the context component

--- src/components/AlgoContext.tsx
+++ src/components/AlgoContext.tsx
@@ -1,7 +1,26 @@
+'use client'
+import { useState } from "react"
+
 interface props {
     children: React.ReactNode
 }
+interface Settings {
+  sortType: 'merge srort' | 'insertion sort';
+  arrayLen : number;
+  delay: number;
+}
+const initialVal = {
+  sortType: 'merge srort',
+  arrayLen : 25,
+  delay: 15,
+}
+
+interface SettingsContext {
+  settings: Settings;
+  setSettings? : React.Dispatch<React.SetStateAction<Settings>>
+}
 const AlgoContext : React.FC<props> = ({children})=> {
+  const [settings,setSettings] = useState<Settings>()
   return (
     <div>{children}</div>
   )
